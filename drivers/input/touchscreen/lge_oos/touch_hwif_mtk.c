0734df714d3a3a6340851fc35ef9d4d6d4c6b2a0
Tibixh <tibi.nemeth12@gmail.com>
2026-06-23 04:18:39 +0200

    mtk: remove recovery mode check in touchscreen driver this is needed to get touch working in recovery

--- drivers/input/touchscreen/lge_oos/touch_hwif_mtk.c
+++ drivers/input/touchscreen/lge_oos/touch_hwif_mtk.c
@@ -335,11 +335,9 @@ int touch_check_boot_mode(struct device *dev)
 
 	/* 2 = RECOVERY_BOOT, 15 == CHARGERLOGO_BOOT, defined only in LK   */
 	boot_mode = get_boot_mode();
-	 if (boot_mode == RECOVERY_BOOT) {
-		ret = TOUCH_RECOVERY_MODE;
-		return ret;
+
 #if defined(CONFIG_LGE_PM_CHARGERLOGO)
-	} else if (boot_mode == CHARGER_BOOT) {
+	if (boot_mode == CHARGER_BOOT) {
 		ret = TOUCH_CHARGER_MODE;
 		return ret;
 #endif
